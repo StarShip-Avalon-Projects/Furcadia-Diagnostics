249472cef510ba50abe60f3a5550f17d8d591592
Jeremy Trombley <gerolkae@hotmail.com>
2017-09-10 21:55:41 -0700

    Update Build System

---
+++ Backup SRC.cmd
@@ -0,0 +1,13 @@
+
+git fetch --all
+IF "%~1"=="" GOTO BuildAll
+IF "%~1"=="VersionBump" GOTO VersionBump
+
+:VersionBump
+msbuild /t:IncrementVersions;BuildAll  Solution.build
+goto :End
+
+:BuildAll
+msbuild /t:BuildAll  Solution.build
+
+:End
--- FurcadiaAccountLogin/SimpleHTTPWebRequest.cs
+++ FurcadiaAccountLogin/SimpleHTTPWebRequest.cs
@@ -92,9 +92,9 @@ namespace AccountTest
             // txtUrl
             // 
             this.txtUrl.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUrl.Location = new System.Drawing.Point(39, 4);
+            this.txtUrl.Location = new System.Drawing.Point(62, 6);
             this.txtUrl.Name = "txtUrl";
-            this.txtUrl.Size = new System.Drawing.Size(376, 21);
+            this.txtUrl.Size = new System.Drawing.Size(602, 27);
             this.txtUrl.TabIndex = 1;
             // 
             // txtHTML
@@ -103,90 +103,90 @@ namespace AccountTest
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txtHTML.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtHTML.Location = new System.Drawing.Point(0, 99);
+            this.txtHTML.Location = new System.Drawing.Point(0, 145);
             this.txtHTML.Multiline = true;
             this.txtHTML.Name = "txtHTML";
             this.txtHTML.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtHTML.Size = new System.Drawing.Size(716, 201);
+            this.txtHTML.Size = new System.Drawing.Size(718, 155);
             this.txtHTML.TabIndex = 6;
             this.txtHTML.WordWrap = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(674, 310);
+            this.button1.Location = new System.Drawing.Point(1078, 453);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(3, 2);
+            this.button1.Size = new System.Drawing.Size(5, 3);
             this.button1.TabIndex = 8;
             this.button1.Text = "button1";
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(12, 7);
+            this.label1.Location = new System.Drawing.Point(19, 10);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(22, 15);
+            this.label1.Size = new System.Drawing.Size(35, 22);
             this.label1.TabIndex = 9;
             this.label1.Text = "Url:";
             // 
             // cmdGo
             // 
-            this.cmdGo.Location = new System.Drawing.Point(423, 4);
+            this.cmdGo.Location = new System.Drawing.Point(677, 6);
             this.cmdGo.Name = "cmdGo";
-            this.cmdGo.Size = new System.Drawing.Size(69, 22);
+            this.cmdGo.Size = new System.Drawing.Size(110, 32);
             this.cmdGo.TabIndex = 2;
             this.cmdGo.Text = "&Go";
             this.cmdGo.Click += new System.EventHandler(this.cmdGo_Click);
             // 
             // txtBoxPassword
             // 
-            this.txtBoxPassword.Location = new System.Drawing.Point(450, 38);
+            this.txtBoxPassword.Location = new System.Drawing.Point(720, 56);
             this.txtBoxPassword.Name = "txtBoxPassword";
-            this.txtBoxPassword.Size = new System.Drawing.Size(191, 20);
+            this.txtBoxPassword.Size = new System.Drawing.Size(306, 26);
             this.txtBoxPassword.TabIndex = 4;
             // 
             // txtBoxUser
             // 
-            this.txtBoxUser.Location = new System.Drawing.Point(138, 38);
+            this.txtBoxUser.Location = new System.Drawing.Point(221, 56);
             this.txtBoxUser.Name = "txtBoxUser";
-            this.txtBoxUser.Size = new System.Drawing.Size(216, 20);
+            this.txtBoxUser.Size = new System.Drawing.Size(345, 26);
             this.txtBoxUser.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Location = new System.Drawing.Point(19, 60);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(120, 13);
+            this.label2.Size = new System.Drawing.Size(179, 20);
             this.label2.TabIndex = 13;
             this.label2.Text = "Account E-Mail Address";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(380, 41);
+            this.label3.Location = new System.Drawing.Point(608, 60);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.Size = new System.Drawing.Size(78, 20);
             this.label3.TabIndex = 14;
             this.label3.Text = "Password";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 76);
+            this.label4.Location = new System.Drawing.Point(19, 111);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.Size = new System.Drawing.Size(62, 20);
             this.label4.TabIndex = 16;
             this.label4.Text = "Api Key";
             // 
             // txtBoxApiKey
             // 
-            this.txtBoxApiKey.Location = new System.Drawing.Point(61, 73);
+            this.txtBoxApiKey.Location = new System.Drawing.Point(98, 107);
             this.txtBoxApiKey.Name = "txtBoxApiKey";
-            this.txtBoxApiKey.Size = new System.Drawing.Size(293, 20);
+            this.txtBoxApiKey.Size = new System.Drawing.Size(468, 26);
             this.txtBoxApiKey.TabIndex = 5;
             // 
             // SimpleHttpWebRequest
             // 
-            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.AutoScaleBaseSize = new System.Drawing.Size(8, 19);
             this.ClientSize = new System.Drawing.Size(713, 300);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.txtBoxApiKey);
