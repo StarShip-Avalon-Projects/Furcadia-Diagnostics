b5e47c5dee38b683c0e419350dfcad068bda370b
Jeremy Trombley <gerolkae@hotmail.com>
2017-09-12 11:58:27 -0700

    Auto Version Update

--- Backup SRC.cmd
+++ Backup SRC.cmd
@@ -1,24 +1,59 @@
 
 git pull
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
 git submodule update -f --merge
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
 git submodule foreach "git pull"
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
 IF "%~1"=="" GOTO BuildAll
 IF "%~1"=="VersionBump" GOTO VersionBump
 
 :VersionBump
 msbuild /t:IncrementVersions;BuildAll  Solution.build
-goto :End
-
+set BUILD_STATUS=%ERRORLEVEL% 
+if %BUILD_STATUS%==0 goto end 
+if not %BUILD_STATUS%==0 goto fail 
+ 
 :BuildAll
 msbuild /t:BuildAll  Solution.build
+set BUILD_STATUS=%ERRORLEVEL% 
+if %BUILD_STATUS%==0 goto end 
+if not %BUILD_STATUS%==0 goto fail 
+ 
+:fail 
+pause 
+exit /b 1
 
 :End
 git add --all
-git commit -m"Update docs" --all
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
+git commit -m"Auto Version Update" --all
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
 
 git submodule foreach "git add --all"
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
 git submodule foreach "git commit -m'Auto Update SubModules'-a"
-git submodule foreach "git push --all"
-git push --all --recurse-submodules=on-demand
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
+git submodule foreach "git push -f origin HEAD:master"
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
+git push -f --all --recurse-submodules=on-demand
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
 
 git request-pull master https://github.com/StarShip-Avalon-Projects/Furcadia-Diagnostics.git
