7a24c9fe0702ab47fdfe488407cfd455bb825031
Jeremy Trombley <gerolkae@hotmail.com>
2017-09-22 16:06:36 -0700

    Auto Version Update

--- Backup SRC.cmd
+++ Backup SRC.cmd
@@ -39,17 +39,20 @@ exit /b 1
 :End
 git add --all
 set GIT_STATUS=%ERRORLEVEL% 
-if not %GIT_STATUS%==0 goto fail 
+if not %GIT_STATUS%==0 goto eof 
 
-git commit -ma"Auto Version Update" --all
+git commit -m"Auto Version Update" --all
 set GIT_STATUS=%ERRORLEVEL% 
-if not %GIT_STATUS%==0 goto fail 
+if not %GIT_STATUS%==0 goto eof 
+
 
 git submodule foreach "git add --all"
 set GIT_STATUS=%ERRORLEVEL% 
-if not %GIT_STATUS%==0 goto fail 
+if not %GIT_STATUS%==0 goto eof 
 
 git submodule foreach "git commit -ma'Auto Update SubModules'"
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto eof 
 
 git push -f --all --recurse-submodules=on-demand
 set GIT_STATUS=%ERRORLEVEL% 
