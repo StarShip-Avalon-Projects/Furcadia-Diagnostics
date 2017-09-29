96bf7fa8ad4e21054eea55d6b70d5e2ce497f449
Jeremy Trombley <gerolkae@hotmail.com>
2017-09-29 13:08:19 -0700

    Auto Version Update

--- Backup SRC.cmd
+++ Backup SRC.cmd
@@ -1,35 +1,24 @@
 call UpdateSrc.cmd
 
-<<<<<<< HEAD
-=======
-git pull
-git submodule update -f --merge
->>>>>>> 97b9aad04c4c5e3b66bd4d56913d497d315f7622
 IF "%~1"=="" GOTO BuildAll
 IF "%~1"=="VersionBump" GOTO VersionBump
 
 :VersionBump
 msbuild /t:IncrementVersions;BuildAll  Solution.build
 set BUILD_STATUS=%ERRORLEVEL% 
-<<<<<<< HEAD
 if %BUILD_STATUS%==0 goto BuildRelease 
-=======
-if %BUILD_STATUS%==0 goto end 
->>>>>>> 97b9aad04c4c5e3b66bd4d56913d497d315f7622
 if not %BUILD_STATUS%==0 goto fail 
  
 :BuildAll
 msbuild /t:BuildAll  Solution.build
 set BUILD_STATUS=%ERRORLEVEL% 
-<<<<<<< HEAD
 if %BUILD_STATUS%==0 goto BuildRelease 
 if not %BUILD_STATUS%==0 goto fail 
 
 :BuildRelease
  msbuild /t:Build /property:Configuration=Release Solution.build
  set BUILD_STATUS=%ERRORLEVEL% 
-=======
->>>>>>> 97b9aad04c4c5e3b66bd4d56913d497d315f7622
+
 if %BUILD_STATUS%==0 goto end 
 if not %BUILD_STATUS%==0 goto fail 
  
@@ -40,9 +29,6 @@ exit /b 1
 :End
 
 git add --all
-<<<<<<< HEAD
-set GIT_STATUS=%ERRORLEVEL% 
-if not %GIT_STATUS%==0 goto eof 
 
 git submodule foreach "git add --all"
 set GIT_STATUS=%ERRORLEVEL% 
@@ -72,4 +58,4 @@ git submodule foreach "git push -f origin HEAD:master"
 git push -f --all --recurse-submodules=on-demand
 
 git request-pull master https://github.com/StarShip-Avalon-Projects/Furcadia-Diagnostics.git
->>>>>>> 97b9aad04c4c5e3b66bd4d56913d497d315f7622
+
