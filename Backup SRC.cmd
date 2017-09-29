97b9aad04c4c5e3b66bd4d56913d497d315f7622
Jeremy Trombley <gerolkae@hotmail.com>
2017-09-29 12:29:14 -0700

    Update docs

--- Backup SRC.cmd
+++ Backup SRC.cmd
@@ -1,17 +1,32 @@
 
-git pull --all
+git pull
+git submodule update -f --merge
 IF "%~1"=="" GOTO BuildAll
 IF "%~1"=="VersionBump" GOTO VersionBump
 
 :VersionBump
 msbuild /t:IncrementVersions;BuildAll  Solution.build
-goto :End
-
+set BUILD_STATUS=%ERRORLEVEL% 
+if %BUILD_STATUS%==0 goto end 
+if not %BUILD_STATUS%==0 goto fail 
+ 
 :BuildAll
 msbuild /t:BuildAll  Solution.build
+set BUILD_STATUS=%ERRORLEVEL% 
+if %BUILD_STATUS%==0 goto end 
+if not %BUILD_STATUS%==0 goto fail 
+ 
+:fail 
+pause 
+exit /b 1
 
 :End
 git add --all
 git commit -m"Update docs" --all
-git push
+
+git submodule foreach "git add --all"
+git submodule foreach "git commit -m'Auto Update SubModules'-a"
+git submodule foreach "git push -f origin HEAD:master"
+git push -f --all --recurse-submodules=on-demand
+
 git request-pull master https://github.com/StarShip-Avalon-Projects/Furcadia-Diagnostics.git
