aa5602777a8443becf6fb7c2a011fee5ca789c13
Jeremy Trombley <gerolkae@hotmail.com>
2017-11-15 16:23:17 -0700

    Auto Version Update

--- Backup.cmd
+++ Backup.cmd
@@ -24,25 +24,25 @@ exit /b 1
 
 :End
 
-git add --all
+git.exe add --all
 
-git submodule foreach "git add --all"
-set GIT_STATUS=%ERRORLEVEL% 
-if not %GIT_STATUS%==0 goto eof 
+git.exe submodule foreach "git.exe add --all"
+set git.exe_STATUS=%ERRORLEVEL% 
+if not %git.exe_STATUS%==0 goto eof 
 
-git submodule foreach "git commit -m'Auto Update SubModules'"
+git.exe submodule foreach "git.exe commit -m'Auto Update SubModules'"
 
 
-git commit -m"Auto Version Update"
-set GIT_STATUS=%ERRORLEVEL% 
-if not %GIT_STATUS%==0 goto eof 
+git.exe commit -m"Auto Version Update"
+set git.exe_STATUS=%ERRORLEVEL% 
+if not %git.exe_STATUS%==0 goto eof 
 
-git push --recurse-submodules=on-demand
-set GIT_STATUS=%ERRORLEVEL% 
-if not %GIT_STATUS%==0 goto eof
+git.exe push --recurse-submodules=on-demand
+set git.exe_STATUS=%ERRORLEVEL% 
+if not %git.exe_STATUS%==0 goto eof
 
 :pull
-git request-pull master https://github.com/StarShip-Avalon-Projects/Furcadia-Diagnostics.git
+git.exe request-pull master https://git.exehub.com/StarShip-Avalon-Projects/Furcadia-Diagnostics.git.exe
 
 :eof
 exit /b 0
--- UpDateSrc.cmd
+++ UpDateSrc.cmd
@@ -1,13 +1,7 @@
 
-git pull --recurse-submodules=on-demand
+git.exe pull --recurse-submodules=yes
 set GIT_STATUS=%ERRORLEVEL% 
 if not %GIT_STATUS%==0 goto fail 
-
-set GIT_STATUS=%ERRORLEVEL% 
-if not %GIT_STATUS%==0 goto fail
-
-
-
 
 :eof
 exit /b 0
